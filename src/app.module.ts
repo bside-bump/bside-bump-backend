8fb8c7db805ce48fc4a9883095c65f4cadcf51da
do-not-do-that <rl7968@naver.com>
2024-09-29 15:09:35 +0900

    feat : typeorm 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +10,20 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath:
         process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
