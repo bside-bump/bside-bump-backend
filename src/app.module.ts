6969f78522075f9ecab2ebe0e933d80a808ad8af
do-not-do-that <rl7968@naver.com>
2024-09-29 16:19:30 +0900

    feat : 이미지 모듈 설정 및 엔티티 typeorm module 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,38 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PUBLIC_FOLDER_PATH } from './consts/path.const';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import { Category } from './entities/category.entity';
+import { Product } from './entities/product.entity';
+import { Result } from './entities/result.entity';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
+    MulterModule.register({
+      limits: {
+        fileSize: 1000000, // 1MB
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+        if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+          return cb(
+            new BadRequestException('png/jpg/jpeg 파일만 업로드 가능합니다.'),
+            false,
+          );
+        }
+
+        return cb(null, true);
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath:
@@ -21,7 +48,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [],
+        entities: [Category, Product, Result],
         synchronize: true,
       }),
     }),
